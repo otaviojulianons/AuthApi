0bf4661876f8918585ef6d43583b6c39b1bfd409
Otávio Nielsen <otavio.juliano.ns@gmail.com>
2019-11-04 23:07:12 -0300

    Implementado repositório mongo

---
+++ Auth.Infrastructure.MongoDB/Contexts/DBContext.cs
@@ -0,0 +1,55 @@
+﻿using Auth.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Driver;
+using System;
+
+namespace Auth.Infrastructure.MongoDB.Contexts
+{
+    public class DBContext
+    {
+
+        public DBContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("MongoDBConnectionString");
+            var mongoClient = new MongoClient(connectionString);
+            Database = mongoClient.GetDatabase("Auth");
+
+            RegisterTypes();
+            RegisterUserAdmin();
+        }
+
+        private void RegisterTypes()
+        {
+            if (!BsonClassMap.IsClassMapRegistered(typeof(UserDomain)))
+            {
+
+                BsonClassMap.RegisterClassMap<UserDomain>(cm =>
+                {
+                    cm.AutoMap();
+                    cm.MapIdMember(c => c.Id).SetIdGenerator(GuidGenerator.Instance);
+                });
+            }
+        }
+
+        private void RegisterUserAdmin()
+        {
+            var _userCollection = Database.GetCollection<UserDomain>("Users");
+            var admin = _userCollection.Find(x => x.Admin).FirstOrDefault();
+            if(admin == null)
+            {
+                var userAdmin = new UserDomain()
+                {
+                    Id = new Guid(),
+                    Admin = true,
+                    Email = "admin",
+                    Password = "21232f297a57a5a743894a0e4a801fc3",
+                };
+                _userCollection.InsertOne(userAdmin);
+            }
+        }
+
+        public IMongoDatabase Database { get; }
+    }
+}
---
+++ Auth.Infrastructure.MongoDB/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Auth.Domain.Entities;
+using Auth.Domain.Repositories;
+using Auth.Infrastructure.MongoDB.Contexts;
+using MongoDB.Driver;
+
+namespace Auth.Infrastructure.MongoDB.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IMongoCollection<UserDomain> _collection;
+
+        public UserRepository(DBContext context)
+        {
+            _collection = context.Database.GetCollection<UserDomain>("Users");
+        }
+
+        public UserDomain GetUserByEmail(string email)
+        {
+            return _collection.Find(x => x.Email == email).FirstOrDefault();
+        }
+    }
+}
